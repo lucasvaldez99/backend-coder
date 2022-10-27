636e2e7458bc4ea50d8ca5bfb8b5ce06ccfeed65
Lucas Valdez <103047986+lucasvaldez99@users.noreply.github.com>
2022-10-26 23:38:23 -0300

    falta menos

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ class usuario {
     constructor (name , surname , mascotas ,libros  ){
         this.name = name  
         this.surname = surname
-        this.mascotas = mascotas
+        this.mascotas = mascotas = []
         this.libros = libros 
     }
 
+    
      
      getFullName( name , surname ) {
         this.name = name 
@@ -14,12 +15,17 @@ class usuario {
      }    
 
      addMascotas(mascotas){
-            this.mascotas = mascotas
-            return mascotas++
+        this.mascotas = mascotas 
+        return this.mascotas.push(mascotas)
+        
+     }
+
+     countMascotas(){
+        this.mascotas.length(mascotas)
      }
     }
 
  const lucas = new usuario();
- console.log(lucas.getFullName("Lucas","Valdez"))    
- console.log(lucas.addMascotas())
-     
+ console.log(lucas.getFullName("Lucas","Valdez"))   
+ console.log(lucas.addMascotas("tuti")) 
+ console.log(lucas.countMascotas)
