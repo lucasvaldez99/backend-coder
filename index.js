1f171b071375b4f5739161bfae5c8864eca1afb2
Lucas Valdez <103047986+lucasvaldez99@users.noreply.github.com>
2022-10-27 21:46:18 -0300

    funcion addmascotas pero no esta del todo bien

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ class usuario {
     constructor (name , surname , mascotas ,libros  ){
         this.name = name  
         this.surname = surname
-        this.mascotas = mascotas = []
+        this.mascotas = mascotas  
         this.libros = libros 
     }
 
@@ -14,18 +14,26 @@ class usuario {
         return name + " " + surname 
      }    
 
-     addMascotas(mascotas){
-        this.mascotas = mascotas 
-        return this.mascotas.push(mascotas)
-        
+     addMascotas(primera,segunda,tercera){
+            this.mascotas = []
+            this.mascotas.push(primera)
+            this.mascotas.push(segunda)
+            this.mascotas.push(tercera)
+            return this.mascotas
      }
+     
 
      countMascotas(){
-        this.mascotas.length(mascotas)
+               this.mascotas = mascotas 
+              return mascotas.lench
      }
     }
 
  const lucas = new usuario();
  console.log(lucas.getFullName("Lucas","Valdez"))   
- console.log(lucas.addMascotas("tuti")) 
+ console.log(lucas.addMascotas('toby','duki','terry'))
  console.log(lucas.countMascotas)
+ console.log(lucas)
+
+
+
